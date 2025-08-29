3d6cf1f94b4e1f0ba7e592fbd4ad45e71bdacb98
Amrit <111822830+apurewal19@users.noreply.github.com>
2025-08-29 12:46:39 -0700

    Update staff.sql

--- sql/staff.sql
+++ sql/staff.sql
@@ -6,12 +6,8 @@ SELECT
   s.hourly_rate,                                      -- Staff hourly pay rate
   sh.start_time,                                      -- Shift start time
   sh.end_time,                                        -- Shift end time
-  ((HOUR(TIMEDIFF(sh.end_time, sh.start_time)) * 60) +
-   (MINUTE(TIMEDIFF(sh.end_time, sh.start_time)))) / 60 AS hours_in_shift,  -- Hours worked
-  (
-    ((HOUR(TIMEDIFF(sh.end_time, sh.start_time)) * 60) +
-     (MINUTE(TIMEDIFF(sh.end_time, sh.start_time)))) / 60
-  ) * s.hourly_rate AS staff_cost                     -- Total labour cost for the shift
+  ((HOUR(TIMEDIFF(sh.end_time, sh.start_time)) * 60) + (MINUTE(TIMEDIFF(sh.end_time, sh.start_time)))) / 60 AS hours_in_shift,                 -- Hours worked
+  (((HOUR(TIMEDIFF(sh.end_time, sh.start_time)) * 60) + (MINUTE(TIMEDIFF(sh.end_time, sh.start_time)))) / 60) * s.hourly_rate AS staff_cost    -- Total labour cost for the shift
 FROM
   rota r
   LEFT JOIN staff s ON r.staff_id = s.staff_id        -- Match shift to staff member
